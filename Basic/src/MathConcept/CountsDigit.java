0b287cfe16eb1eb610d3463e7cdca768889aed2c
Shailesh <Shaileshsharmasvit@gmail.com>
2024-07-13 10:17:30 -0500

    count digit with log method

--- Basic/src/MathConcept/CountsDigit.java
+++ Basic/src/MathConcept/CountsDigit.java
@@ -3,9 +3,16 @@ package MathConcept;
 public class CountsDigit {
 public static void main(String[] args) {
 int no=7898;  
-System.out.println(count(no));
+System.out.println(count2(no));
 
 
+}
+
+public static int count2(int no) {
+	
+	int cnt=(int) (Math.log10(no)+1);
+	return cnt;
+	
 }
 public static int count(int no) {
 	int cnt=0;
