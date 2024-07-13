4ef7aa87197d3dc9193267f7055655f17081a3e7
Shailesh <Shaileshsharmasvit@gmail.com>
2024-07-13 17:37:34 -0500

    Armstrong and palindrome number implementation

--- Basic/src/MathConcept/CountsDigit.java
+++ Basic/src/MathConcept/CountsDigit.java
@@ -2,10 +2,80 @@ package MathConcept;
 
 public class CountsDigit {
 public static void main(String[] args) {
-int no=1534236469;  
+int no=2;  
 System.out.println(count2(no));
 System.out.println();
 System.out.println(reverse(no));
+System.out.println(palindrome(no));
+System.out.println();
+
+System.out.println(armstrong(no));
+}
+
+public static boolean armstrong(int n) {
+	int len=0;
+	int no=n;
+	while(no!=0) {
+		len++;
+		no/=10;
+	}
+	return getSum(n,len)==n;
+}
+public static int getSum(int no,int l) {
+	int result=0;
+
+	while(no!=0) {
+result+=+Math.pow(no%10, l);
+
+no/=10;
+	}
+return result;
+}
+public static int getSumOfKthPowerOfDigits(int n, int k) {
+    // `result` stores the result of sum of k'th power of each digit.
+   int result = 0;
+
+   // Run until n is not 0
+   while(n != 0) {
+       // Modulo 10 gives us the last digit
+       // Add digit ^ k to the result
+       result += Math.pow(n % 10, k);
+
+       // Remove the last digit.
+       n /= 10;
+   }
+   return result;
+}
+public static boolean isArmstrong(int n) {
+    // Initilize length counter to 0.
+    int length = 0;
+
+    // Get the number of digits in integer `n`.
+    int tempN = n;
+
+    // Get the length of the integer `n`
+    while (tempN != 0) {
+        length++;
+        tempN /= 10;
+    }
+
+    // Return true if Sum of k'th power of digits equals original number.
+    return getSumOfKthPowerOfDigits(n, length) == n;
+}
+
+
+public static boolean palindrome(int s) {
+	int rev=0;
+	int no=s;
+	while(no>0) {
+		int lastDigit=no%10;
+		rev=(rev*10)+lastDigit;
+				no/=10;
+	}
+	if(s==rev)
+		return true;
+	else 
+		return false;
 }
 public static int reverse(int x) {
 boolean isNegative = false;
