6aa066f1b513a5882554a87205cae62fae753941
Shailesh <Shaileshsharmasvit@gmail.com>
2024-07-13 12:20:02 -0500

    Leetcode problem number 7 reverse digit of a number

--- Basic/src/MathConcept/CountsDigit.java
+++ Basic/src/MathConcept/CountsDigit.java
@@ -2,12 +2,35 @@ package MathConcept;
 
 public class CountsDigit {
 public static void main(String[] args) {
-int no=7898;  
+int no=1534236469;  
 System.out.println(count2(no));
-
-
+System.out.println();
+System.out.println(reverse(no));
+}
+public static int reverse(int x) {
+boolean isNegative = false;
+if (x < 0) {
+    isNegative = true;
+    x = -x;
+}
+//1534236469
+int res = 0;
+while (x > 0) { 
+    int digit = x % 10;
+    x /= 10;
+	
+	  if (res > (Integer.MAX_VALUE - digit) / 10) {
+		  
+		  return 0;
+		  
+	  }
+	
+    res = (res * 10) + digit;
 }
 
+return isNegative ? -res : res;    
+	
+}
 public static int count2(int no) {
 	
 	int cnt=(int) (Math.log10(no)+1);
